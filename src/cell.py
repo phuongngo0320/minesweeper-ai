95e9fd748dc780d840b9949a32a97693faad4acf
Phuong <phuongngovan2003@gmail.com>
2024-04-01 14:33:52 +0700

    Add cell string

--- src/cell.py
+++ src/cell.py
@@ -6,3 +6,18 @@ class Cell:
         self.flag = False
         self.mine = False
         # self.marked = False
+        
+    def __repr__(self) -> str:
+        if self.covered:
+            if self.flag:
+                return '>'
+            else:
+                return '_'
+            
+        if self.mine:
+            return '*'
+        
+        # else: number
+        return str(self.number)
+            
+            
